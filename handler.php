3461220527a025cf9fa4d9314901e3c4ae6233e9
Niffecs <Niffecs@gmail.com>
2022-12-14 23:52:01 +0100

    Upload

--- handler.php
+++ handler.php
@@ -1,4 +1,4 @@
-<?php
+<?
 
 // load all vars
 include 'var.php';
@@ -8,12 +8,13 @@ $load = $_GET['db_key'];
 // Store the file name into variable
 $file = $storage_dir . '/' . $load . $content_value . $ext;
 $filename = $load . $ext;
-
 // Header content type
 header('Content-type: application/pdf');
 header('Content-Disposition: inline; filename="' .test_md5($load). '.pdf"');
 header('Content-Transfer-Encoding: binary');
 header('Accept-Ranges: bytes');
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
 
 // Read the file
 @readfile($file);
