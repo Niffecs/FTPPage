5028f178dadca5b06354efff65491eff9b7542b5
Niffecs <Niffecs@gmail.com>
2022-12-15 00:07:42 +0100

    Fix

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ if(preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|c
 <link rel="stylesheet" href="style.css">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image" href="image.png">
+
 <title>Niffecs FTP Manager</title>
 </head>
 <body>
@@ -33,6 +35,7 @@ include("content.php");
 echo "<center>";
 include("session.php");
 echo "</center>";
+include("logger.php");
 ?>
 
 </div>
